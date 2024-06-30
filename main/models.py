514e125a99645192453603e81d4f0c23f84d5a1f
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2024-06-30 13:54:39 +0200

    Rédaction de Readme

--- main/models.py
+++ main/models.py
@@ -10,7 +10,7 @@ class Blog_Articles(models.Model):
     # champs contenu dans le model
     title = models.CharField(max_length= 100, unique = True)
     content = models.CharField(max_length=300, unique = True)
-    # publication_date = models.DateField()
+    # publication_date = models.DateField(auto_now_add=True)
    
     # clé étrangère venant d'un autre table
     author = models.ForeignKey(Author, on_delete= models.DO_NOTHING)
